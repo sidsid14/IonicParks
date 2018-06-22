3a94fb6869e62fd9e9191e6a8d034387b015ad80
sudhanshu <sudhanshu@viosmedical.com>
2018-06-23 00:16:16 +0530

    Added ParkList Page

--- src/pages/park-list/park-list.ts
+++ src/pages/park-list/park-list.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-/*
-  Generated class for the ParkList page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import { ParkData } from '../../app/providers/park-data';
 @Component({
   selector: 'page-park-list',
   templateUrl: 'park-list.html'
 })
 export class ParkListPage {
+  parks: Array<Object> = []
+  constructor(public navCtrl: NavController, public navParams: NavParams, public parkData: ParkData) {
+    parkData.getParks().then(parks => {
+      this.parks = parks;
+    });
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  goParkDetails(theParkData){
+    console.log(theParkData);
+    
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ParkListPage');
