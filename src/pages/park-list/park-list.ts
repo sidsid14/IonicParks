82a1fa590eae8d6abad520e4b4944b01c3c513d4
sudhanshu <sudhanshu@viosmedical.com>
2018-06-24 17:26:57 +0530

    Add Map Markers

--- src/pages/park-list/park-list.ts
+++ src/pages/park-list/park-list.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ParkData } from '../../app/providers/park-data';
+import { Park } from '../../app/interfaces/park';
 @Component({
   selector: 'page-park-list',
   templateUrl: 'park-list.html'
 })
 export class ParkListPage {
-  parks: Array<Object> = []
+  parks: Array<Park> = [];
   constructor(public navCtrl: NavController, public navParams: NavParams, public parkData: ParkData) {
     parkData.getParks().then(parks => {
       this.parks = parks;
--- src/pages/park-map/park-map.ts
+++ src/pages/park-map/park-map.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Platform, NavController, NavParams } from 'ionic-angular';
-
+import { ParkData } from '../../app/providers/park-data';
+import { Park } from '../../app/interfaces/park';
 @Component({
   selector: 'page-park-map',
   templateUrl: 'park-map.html'
 })
 export class ParkMapPage {
   map: google.maps.Map;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public platform:Platform) {
+  parks: Array<Park> = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform:Platform, public parkData:ParkData) {
     this.map = null;
   }
 
@@ -25,6 +28,21 @@ export class ParkMapPage {
       mapTypeControl: false,
       streetViewControl: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    let image = 'assets/img/nps_arrowhead.png';
+    this.parkData.getParks().then(theResult => {
+      this.parks = theResult;
+
+      for(let thePark of this.parks){
+        let parkPos: google.maps.LatLng = new google.maps.LatLng(thePark.lat, thePark.long);
+        let parkMarker: google.maps.Marker = new google.maps.Marker();
+        parkMarker.setPosition(parkPos);
+        parkMarker.setMap(this.map);
+        parkMarker.setIcon(image);
+
+      }
+
     });
 
   }
